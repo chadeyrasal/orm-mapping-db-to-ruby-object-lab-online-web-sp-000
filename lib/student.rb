9881efd50e94127af5f4b7a7d2492b1ecc66bd27
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2019-01-09 10:28:28 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,7 +13,7 @@ class Student
   def self.all
     sql = <<-SQL
       SELECT *
-      FROM songs
+      FROM students
     SQL
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
