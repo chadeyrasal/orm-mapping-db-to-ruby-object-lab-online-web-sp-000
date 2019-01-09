d918385f96e8570e93b2b8d97f408f55f400920d
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2019-01-09 10:26:14 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -42,6 +42,12 @@ class Student
   end
 
   def self.students_below_12th_grade
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade < '12'
+      SQL
+    DB[:conn].execute(sql)
   end
 
   def save
